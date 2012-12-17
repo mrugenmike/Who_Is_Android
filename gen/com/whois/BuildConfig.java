07a21c514aa7e6a234e5ba722024c38b4b55d7d7
Mrugen Deshmukh <mrugenmike@gmail.com>
2012-12-17 23:21:53 +0530

    initiated the project structure

---
+++ gen/com/whois/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.whois;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/whois/HomeScreenActivity.java
@@ -0,0 +1,15 @@
+package com.whois;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
